23ed65a4c9077224f42b4d53040cf12f04b6399a
bmcpt <iaansagar@gmail.com>
2022-08-14 18:48:29 +0530

    init

---
+++ dbcppp_rs_sys/build.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let dst = cmake::Config::new("..")
+        .define("BUILD_STATIC_LIBS", "ON")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_TESTING", "OFF")
+        .cxxflag("-lstdc++")
+        .always_configure(true)
+        .build();
+    println!("cargo:rustc-link-search=native={}/build/src/libdbcppp", dst.display());
+    println!("cargo:rustc-link-lib=static=dbcppp");
+    println!("cargo:rustc-link-arg=-lstdc++");
+
+    let header = "../include/dbcppp/CApi.h";
+    println!("cargo:rerun-if-changed={}", header);
+    let bindings = bindgen::Builder::default()
+        .header(header)
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ dbcppp_rs_sys/src/bin/ex1.rs
@@ -0,0 +1,31 @@
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+use std::ptr::null;
+use dbcppp_rs_sys::*;
+
+fn str_to_cstring(s: &str) -> CString {
+    CString::new(s).unwrap()
+}
+
+fn chars_to_string(ptr: *const c_char) -> String {
+    unsafe { CStr::from_ptr(ptr).to_str().unwrap().to_string() }
+}
+
+fn main() {
+    unsafe {
+        f();
+    }
+}
+
+unsafe fn f() {
+    let dbc = dbcppp_NetworkLoadDBCFromFile(
+        str_to_cstring("../my.tmp.dir/test.dbc").as_ptr(),
+    );
+    if dbc == null() {
+        println!("invalid dbc file");
+    }
+    for msg in (0..dbcppp_NetworkMessages_Size(dbc))
+        .map(|idx| dbcppp_NetworkMessages_Get(dbc, idx)) {
+        println!("{}: {}", chars_to_string(dbcppp_MessageName(msg)), dbcppp_MessageId(msg));
+    }
+}
